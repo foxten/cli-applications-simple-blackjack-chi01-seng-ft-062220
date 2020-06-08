54327fe4103bdcc1a425eb9dc0a8d729d828f9b0
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-08 21:57:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,6 +57,8 @@ def runner
   display_card_total(card_total)
   if card_total> 21
   end_game(card_total)
+  elsif card_total == 21
+  puts "You've won!"
   else 
     if card_total < 21
     card_total_2 = hit?(card_total)
