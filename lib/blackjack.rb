963c76878607669c45e50341a268b5a49a9d5082
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-08 21:08:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,11 +32,11 @@ def hit?(total)
   prompt_user
   get_user_input
     if get_user_input = "s"
-      return total
+      total
     elsif get_user_input = "h"
       deal_card
       total += deal_card
-      return total
+      total
     end
 end
 
